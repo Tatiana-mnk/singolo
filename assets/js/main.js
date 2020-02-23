baf7d4a77723a5ea2576d773d6ee76fe7f64ee57
tatiana.mankevich93@gmail.com <139408Vt5020>
2020-02-24 01:40:28 +0300

    docs: add assets folder

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.slider__block').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        // autoplay: true,
+        arrows: true,
+        autoplaySpeed: 3000,
+    });
+});
